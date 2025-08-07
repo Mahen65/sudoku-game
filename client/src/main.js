00f91daeb89243427a7840059f8d2d0eabb95934
Asiri Somarathne <asiri@twire.se>
2025-08-07 12:10:12 +0530

    changed the main.js comments

--- client/src/main.js
+++ client/src/main.js
@@ -4,7 +4,8 @@ import router from './router'
 import axios from 'axios'
 
 axios.defaults.baseURL = '/api'
-
+// Set up axios to use the base URL for API requests
 const app = createApp(App)
 app.config.globalProperties.$http = axios
 app.use(router).mount('#app')
+
